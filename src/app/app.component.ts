ebee77a455866d45016345c18a957cf46949a1f0
kiran <Kieanivare@gmail.com>
2023-11-27 20:09:22 +0530

    task 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'task2';
+  saveFormData() {
+    const form = document.getElementById('myForm') as HTMLFormElement;
+    const formData = new FormData(form);
+
+    // Accessing form data values and constructing an object
+    const formValues: { [key: string]: string } = {};
+    formData.forEach((value, key) => {
+      formValues[key] = value as string;
+    });
+
+    // Logging form data to the console
+    console.log('Form Data:', formValues);
+  }
 }
